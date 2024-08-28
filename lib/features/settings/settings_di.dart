1831346723675b0395706f587a5c9a3d3e5540a9
toastmanager <mrbonkyou@gmail.com>
2024-08-28 21:21:13 +0900

    settings dependency injection

---
+++ lib/features/settings/settings_di.dart
@@ -0,0 +1,22 @@
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather_app/features/settings/data/datasources/local/settings_local_datasource.dart';
+import 'package:weather_app/features/settings/data/repositories/settings_repository_impl.dart';
+import 'package:weather_app/features/settings/domain/repositories/settings_repository.dart';
+import 'package:weather_app/features/settings/domain/usecases/get_settings.dart';
+import 'package:weather_app/features/settings/presentation/bloc/settings_bloc.dart';
+import 'package:weather_app/injection.dart';
+
+Future<void> settingsDIInit() async {
+  locator.registerFactory<SettingsBloc>(
+      () => SettingsBloc(locator<GetSettings>()));
+
+  locator.registerLazySingleton<GetSettings>(
+      () => GetSettings(repository: locator<SettingsRepository>()));
+
+  locator.registerLazySingleton<SettingsRepository>(() =>
+      SettingsRepositoryImpl(
+          settingsLocalDataSource: locator<SettingsLocalDataSource>()));
+
+  locator.registerLazySingleton<SettingsLocalDataSource>(
+      () => SettingsLocalDatasourceImpl(prefs: locator<SharedPreferences>()));
+}
--- lib/injection.dart
+++ lib/injection.dart
@@ -1,11 +1,17 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/core/routes/router.dart';
+import 'package:weather_app/features/settings/settings_di.dart';
 import 'package:weather_app/features/weather/weather_di.dart';
 
 final locator = GetIt.instance;
 
 Future<void> initDI() async {
+  locator.registerLazySingletonAsync<SharedPreferences>(() => SharedPreferences.getInstance());
+  await locator.isReady<SharedPreferences>();
+  await settingsDIInit();
+
   locator.registerLazySingleton<AppRouter>(() => AppRouter());
 
   locator.registerLazySingleton<Dio>(() => Dio());
