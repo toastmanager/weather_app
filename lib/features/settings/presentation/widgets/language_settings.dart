85a857761c746f73c121ed9bfc09883fbfd58b76
toastmanager <mrbonkyou@gmail.com>
2024-08-28 19:50:31 +0900

    Fixed wrong naming

--- lib/features/settings/presentation/widgets/language_settings.dart
+++ lib/features/settings/presentation/widgets/language_settings.dart
@@ -8,58 +8,8 @@ class LanguageSettings extends StatefulWidget {
 }
 
 class _LanguageSettingsState extends State<LanguageSettings> {
-  late TextEditingController latitudeController;
-  late TextEditingController longitudeController;
-
-  @override
-  void initState() {
-    latitudeController = TextEditingController();
-    longitudeController = TextEditingController();
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    latitudeController.dispose();
-    longitudeController.dispose();
-    super.dispose();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text(
-          'Location',
-          style: Theme.of(context)
-              .textTheme
-              .titleLarge
-              ?.copyWith(fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 24,
-        ),
-        TextField(
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            label: Text('Latitude')
-          ),
-          keyboardType: TextInputType.number,
-          controller: latitudeController,
-        ),
-        const SizedBox(
-          height: 12,
-        ),
-        TextField(
-          decoration: const InputDecoration(
-            border: OutlineInputBorder(),
-            label: Text('Longitude')
-          ),
-          keyboardType: TextInputType.number,
-          controller: longitudeController,
-        ),
-      ],
-    );
+    return Placeholder();
   }
 }
--- lib/features/settings/presentation/widgets/location_settings.dart
+++ lib/features/settings/presentation/widgets/location_settings.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
 
-class LocationSettings extends StatelessWidget {
+class LocationSettings extends StatefulWidget {
   const LocationSettings({super.key});
 
   @override
+  State<LocationSettings> createState() => _LocationSettingsState();
+}
+
+class _LocationSettingsState extends State<LocationSettings> {
+  late TextEditingController latitudeController;
+  late TextEditingController longitudeController;
+
+  @override
+  void initState() {
+    latitudeController = TextEditingController();
+    longitudeController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    latitudeController.dispose();
+    longitudeController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Location',
+          style: Theme.of(context)
+              .textTheme
+              .titleLarge
+              ?.copyWith(fontWeight: FontWeight.bold),
+        ),
+        const SizedBox(
+          height: 24,
+        ),
+        TextField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            label: Text('Latitude')
+          ),
+          keyboardType: TextInputType.number,
+          controller: latitudeController,
+        ),
+        const SizedBox(
+          height: 12,
+        ),
+        TextField(
+          decoration: const InputDecoration(
+            border: OutlineInputBorder(),
+            label: Text('Longitude')
+          ),
+          keyboardType: TextInputType.number,
+          controller: longitudeController,
+        ),
+      ],
+    );
   }
 }
