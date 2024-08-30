f298a755255372202e1f75e79eba96d30821394c
toastmanager <mrbonkyou@gmail.com>
2024-08-31 08:31:28 +0900

    remove shared_preferences

--- lib/injection.dart
+++ lib/injection.dart
@@ -1,6 +1,5 @@
 import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:weather_app/core/routes/router.dart';
 import 'package:weather_app/features/settings/settings_di.dart';
 import 'package:weather_app/features/weather/weather_di.dart';
@@ -9,12 +8,10 @@ import 'package:weather_app/objectbox.dart';
 final locator = GetIt.instance;
 
 Future<void> initDI() async {
-  locator.registerLazySingletonAsync<SharedPreferences>(() => SharedPreferences.getInstance());
-  await locator.isReady<SharedPreferences>();
-  await settingsDIInit();
-
   locator.registerLazySingletonAsync<ObjectBox>(() => ObjectBox.create());
   await locator.isReady<ObjectBox>();
+
+  await settingsDIInit();
 
   locator.registerLazySingleton<AppRouter>(() => AppRouter());
 
