a8cd0f34a7b2355e87e96e0bccfd7541e1df50bf
toastmanager <mrbonkyou@gmail.com>
2024-08-31 19:30:36 +0900

    Revert "primary color picker placeholders"
    This reverts commit 75223e44129cb9bc2a910b80004cc81c7a66e680.

--- lib/features/settings/presentation/widgets/primary_color_settings.dart
+++ lib/features/settings/presentation/widgets/primary_color_settings.dart
@@ -1,42 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:weather_app/features/settings/presentation/widgets/primary_color_choose_button.dart';
 
 class PrimaryColorSettings extends StatelessWidget {
   const PrimaryColorSettings({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Text('Primary color',
-            style: Theme.of(context)
-                .textTheme
-                .titleLarge
-                ?.copyWith(fontWeight: FontWeight.bold)),
-        const SizedBox(
-          height: 24,
-        ),
-        Wrap(
-          children: [
-            PrimaryColorChooseButton(
-              colorHex: 0xffE71492,
-            ),
-            const SizedBox(width: 16),
-            PrimaryColorChooseButton(
-              colorHex: 0xff3E7DC8,
-            ),
-            const SizedBox(width: 16),
-            PrimaryColorChooseButton(
-              colorHex: 0xff653EC8,
-            ),
-            const SizedBox(width: 16),
-            PrimaryColorChooseButton(
-              colorHex: 0xff3EC85C,
-            ),
-          ],
-        )
-      ],
-    );
+    return const Placeholder();
   }
 }
